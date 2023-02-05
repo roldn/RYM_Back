d5cd92facac62f34b037512fb50788383084cdb0
Roldán Francisco <98569621+roldn@users.noreply.github.com>
2023-02-05 18:41:08 -0300

    New connection w/MongoDB - not working

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import cors from 'cors';
 import express from 'express';
 import * as dotenv from "dotenv";
-import mongoose from 'mongoose';
 import helmet from "helmet";
 import * as crud from '../src/router/character-crud';
+import { PrismaClient } from '@prisma/client';
 
 
 dotenv.config();
@@ -15,11 +15,36 @@ if (!process.env.PORT) {
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
 
-export const app = express();
+const app = express();
+const prisma = new PrismaClient()
+
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+async function main() {
+
+  await prisma.$connect()
+
+    const post = await prisma.character.create({
+      data: {
+        name: 'Morty',
+        status: 'dead',
+        gender: 'male'
+      },
+    });
+    console.log(post)
+}
+main().then(
+  async () => {
+    await prisma.$disconnect()
+  }
+).catch(async (e) => {
+  console.error(e)
+  await prisma.$disconnect()
+  process.exit(1)
+})
 
 const server = app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
@@ -32,3 +57,5 @@ app.get('/characters', crud.getAll);
 app.post('/create', crud.create);
 app.post('/uptade', crud.update);
 app.post('/remove', crud.remove);
+
+
